becb96296c1b7a4cc4de4b64e3537f7c1ff48aa5
Jordan Garcia Jara <jordangarciajara@gmail.com>
2025-09-06 14:47:38 +0000

    ponme este logo de esta url en los logos de la web: https://iili.io/KnvX

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,21 +1,17 @@
 import type { SVGProps } from "react";
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
 
-export function MacOutLogo(props: SVGProps<SVGSVGElement>) {
+export function MacOutLogo(props: SVGProps<SVGSVGElement> & { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z" />
-      <path d="M7 15l2-3-2-3" />
-      <path d="M17 15l-2-3 2-3" />
-      <path d="M9 12h6" />
-    </svg>
+    <div className={cn("relative", props.className)}>
+        <Image
+            src="https://iili.io/KnvXOSR.png"
+            alt="Mac-Out Logo"
+            fill
+            className="object-contain"
+            sizes="10vw"
+        />
+    </div>
   );
 }
