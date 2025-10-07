779c4643620ca614b719ecb558bddfc0c0bc5045
Jordan Garcia Jara <jordangarciajara@gmail.com>
2025-10-07 20:44:19 +0000

    Ajustar el diseño de la sección principal para dispositivos oredenadores

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -9,19 +9,19 @@ export function Hero() {
         alt="Logo de Macout sobre un fondo de cubos azules y negros"
         fill
         priority
-        className="object-cover md:object-center"
+        className="object-cover object-center"
         data-ai-hint="logo company background"
       />
       <div className="absolute inset-0 bg-black/50" />
-      <div className="relative z-10 container mx-auto px-4 flex flex-col justify-end md:justify-center h-full pb-12 md:pb-24">
-        <div className="max-w-3xl text-center mx-auto">
-          <h1 className="font-headline text-2xl sm:text-5xl lg:text-6xl font-bold tracking-tight">
+      <div className="relative z-10 container mx-auto px-4 flex flex-col items-center justify-end h-full pb-12 md:pb-0 md:pt-48">
+        <div className="max-w-3xl text-center">
+          <h1 className="font-headline text-xl sm:text-2xl lg:text-3xl font-bold tracking-tight">
               Tu Apple reacondicionado.
               <span className="block text-primary mt-1 md:mt-2">
                  Calidad y ahorro.
               </span>
           </h1>
-          <p className="mt-4 md:mt-6 text-base md:text-lg text-gray-200">
+          <p className="mt-4 md:mt-6 text-sm md:text-base text-gray-200">
               Equipos 100% verificados, con garantía y al mejor precio. Directamente desde el corazón de Sevilla.
           </p>
         </div>
