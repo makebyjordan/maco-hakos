c266d5a5d01c6ee78c27c8c40b785106eda9ef7d
Jordan Garcia Jara <jordangarciajara@gmail.com>
2025-10-16 16:50:55 +0000

    DUPLICATE ESTE LINK COMPONENT EN EL HEADER ENCIMA DEL TEXTO (_for elemen

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { AnimatedSection } from "../animated-section";
+import Link from "next/link";
+import { MacOutLogo } from "../icons";
 
 export function Hero() {
   return (
@@ -14,7 +16,15 @@ export function Hero() {
       />
       <div className="absolute inset-0 bg-black/50" />
       <div className="relative z-10 container mx-auto px-4 flex flex-col items-center justify-center h-full">
-        <div className="max-w-3xl text-center pt-[270px]">
+        <div className="max-w-3xl text-center">
+           <Link href="/" className="flex items-center justify-center gap-4 mb-8">
+              <MacOutLogo className="h-24 w-24 text-white" />
+              <div className="font-headline text-5xl font-bold">
+                <div className="-mb-2">MAC</div>
+                <div className="h-1 bg-white w-full"></div>
+                <div className="-mt-2">OUT</div>
+              </div>
+            </Link>
           <h1 className="font-headline text-xl sm:text-2xl lg:text-3xl font-bold tracking-tight">
               Tu Apple reacondicionado.
               <span className="block text-primary mt-1 md:mt-2">
