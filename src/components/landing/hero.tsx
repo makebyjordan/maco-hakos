e676973e661761f18afa3520407bddee117cfbb0
Jordan Garcia Jara <jordangarciajara@gmail.com>
2025-11-06 11:30:32 +0000

    Pon la manzana al lado izquierdo de "MAC OUT" con 1cm de gap, y centra e

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -17,8 +17,8 @@ export function Hero() {
       <div className="absolute inset-0 bg-black/60" />
       <div className="relative container mx-auto flex flex-col items-center justify-center h-full px-4">
         <div className="max-w-4xl mx-auto mb-8 text-center">
-           <Link href="/" className="flex flex-col sm:flex-row items-center justify-center">
-              <div className="relative h-24 w-24 sm:h-40 sm:w-40">
+           <Link href="/" className="flex items-center justify-center gap-4">
+              <div className="relative h-24 w-24 sm:h-32 sm:w-32">
                   <Image
                       src="https://iili.io/KQ5UldN.png"
                       alt="Macout Logo Manzana"
@@ -26,7 +26,7 @@ export function Hero() {
                       className="object-contain"
                   />
               </div>
-              <div className="relative h-[100px] w-[260px] sm:h-[250px] sm:w-[650px] sm:ml-4">
+              <div className="relative h-[100px] w-[260px] sm:h-[150px] sm:w-[325px]">
                 <Image
                     src="https://iili.io/KQ5Y5vt.png"
                     alt="Macout Logotipo"
@@ -35,10 +35,10 @@ export function Hero() {
                 />
               </div>
             </Link>
-          <h1 className="font-headline pt-4 sm:pt-8 text-3xl sm:text-5xl font-bold tracking-tight">
+          <h1 className="font-headline pt-8 text-3xl sm:text-5xl font-bold tracking-tight">
               Tu Apple reacondicionado.
           </h1>
-          <p className="font-bold text-2xl md:text-5xl text-primary mt-4">Calidad y Ahorro</p>
+          <p className="font-bold text-4xl md:text-6xl text-primary mt-4">Calidad y Ahorro</p>
           <p className="mt-4 text-base md:text-xl text-gray-200">
               Equipos 100% verificados,
               <br />
